1e54aa7eddb3961440207968da82af659d246a48
xiangshuyue <xiangshuyue@outlook.com>
2023-09-29 18:35:45 +0800

    修改主游戏文件

--- plane_war.py
+++ plane_war.py
@@ -13,9 +13,12 @@
 import pygame
 import configparser
 from interface import Background
+from player import HeroPlane
+from bullet import Bullet
+
 
 config = configparser.ConfigParser()
-config.read('game.ini')
+config.read('game.ini', encoding='utf-8')
 
 width = config.getint('Settings', 'width')
 height = config.getint('Settings', 'height')
@@ -43,13 +46,38 @@ bg1 = Background(0)
 bg2 = Background(-height)
 bg_group = pygame.sprite.Group(bg1, bg2)
 
+# 创建玩家飞机
+heroplane = HeroPlane()
+
+# 标识玩家飞机图像切换事件
+IMAGE_FLAG = pygame.USEREVENT + 1
+pygame.time.set_timer(IMAGE_FLAG, 100)
+switch_flag = True
+
+# 发射子弹事件
+FIRE = pygame.USEREVENT + 2
+pygame.time.set_timer(FIRE, 200)
+
+bullets = pygame.sprite.Group()
+
 while True:
     clock.tick(fps)
-    for event in  pygame.event.get():
+    key_pressed = pygame.key.get_pressed()
+    for key in [pygame.K_UP, pygame.K_DOWN, pygame.K_LEFT, pygame.K_RIGHT]:
+        if key_pressed[key]:
+            heroplane.move(key)
+    for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
-        # if event.type == TEST:
-        #     print('Test Event')
+        if event.type == IMAGE_FLAG:
+            if switch_flag:
+                switch_flag = False
+            else:
+                switch_flag = True
+        if event.type == FIRE:
+            bullet = Bullet(heroplane.bullet_type, heroplane.image_rect.midtop)
+            bullets.add(bullet)
     bg_group.update(screen)
-    pygame.display.flip()
-    
+    heroplane.update(screen, switch_flag)
+    bullets.update(screen, bullets)
+    pygame.display.update()
